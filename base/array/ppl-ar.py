0d454927c45d8da46a7d2e9cc31a1d3c5cd2b57e
Criz <crizmo595@gmail.com>
2022-04-09 15:38:48 +0530

    Create ppl-ar.py

---
+++ base/array/ppl-ar.py
@@ -0,0 +1,35 @@
+ppl = ["criz", "speedy_bat", "inforgas"]
+print(ppl)
+
+ppl.append("jim")
+print(ppl)
+
+ppl.copy()
+print(ppl)
+
+ppl.count("jim")
+print(ppl)
+
+ppl.extend(["jim", "jim"])
+print(ppl)
+
+ppl.index("speedy_bat")
+print(ppl)
+
+ppl.insert(1, "jim")
+print(ppl)
+
+ppl.pop()
+print(ppl)
+
+ppl.remove("speedy_bat")
+print(ppl)
+
+ppl.reverse()
+print(ppl)
+
+ppl.sort()
+print(ppl)
+
+ppl.clear()
+print(ppl)
