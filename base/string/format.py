489479d50b12957dda6963deee5ce2b4b9619910
Criz <crizmo595@gmail.com>
2022-04-13 18:28:18 +0530

    Create format.py

---
+++ base/string/format.py
@@ -0,0 +1,15 @@
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
