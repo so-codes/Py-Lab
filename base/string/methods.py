293d20d4ddf7381b68c7b9eb085acd4c829921b2
Criz <crizmo595@gmail.com>
2022-04-13 18:28:21 +0530

    Create methods.py

---
+++ base/string/methods.py
@@ -0,0 +1,58 @@
+line = "hi i am criz !"
+
+x = line.capitalize()
+print(x)
+
+x = line.casefold()
+print(x)
+
+x = line.center(50, "*")
+print(x)
+
+x = line.count("i")
+print(x)
+
+x = line.encode()
+print(x)
+
+x = line.endswith("!")
+print(x)
+
+x = line.expandtabs(tabsize=4)
+print(x)
+
+x = line.find("i")
+print(x)
+
+x = line.format()
+print(x)
+
+x = line.format_map(vars())
+print(x)
+
+x = line.index("i")
+print(x)
+
+x = line.isalnum()
+print(x)
+
+x = line.isalpha()
+print(x)
+
+x = line.isdecimal()
+print(x)
+
+x = line.isdigit()
+print(x)
+
+x = line.isidentifier()
+print(x)
+
+x = line.islower()
+print(x)
+
+x = line.isnumeric()
+print(x)
+
+x = line.isprintable()
+print(x)
