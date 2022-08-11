e130142b2e08f93d149b40d7afef8877c606a32e
crizmo <crizmo595@gmail.com>
2022-08-11 19:32:45 +0530

    ✨ : better look

--- base/basics/casting.py
+++ base/basics/casting.py
@@ -1,3 +1,21 @@
+# ints
+x = int(3)
+y = int(3.0)
+z = int("3")
+
+# floats
+x = float(3)
+y = float(3.0)
+z = float("3")
+
+# complex
+x = complex(3)
+y = complex(3.0)
+z = complex("3")
+
+# strings
 x = str(3)
-y = int(3)
-z = float(3)
+y = str(3.0)
+z = str(3j)
+
+print(x, y, z)
