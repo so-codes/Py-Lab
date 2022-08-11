e6775f1361cae1726bb2ddeae94ce0c2271a4ca8
crizmo <crizmo595@gmail.com>
2022-08-11 19:33:30 +0530

    📚 : datatypes & operators notes

---
+++ base/basics/dypes.py
@@ -0,0 +1,39 @@
+# Immutable data types
+
+# 1. 
+# Immutable data types are those that cannot be changed.
+# Immutable data types are:
+# 1. Numbers
+# 2. Strings
+# 3. Tuples
+
+# In numbers:-
+# 1. int
+# 2. float
+# 3. complex
+
+# In strings:-
+# 1. str
+# 2. bytes
+# 3. bytearray
+
+# In tuples:-
+# 1. tuple
+# 2. frozenset
+
+# 2.
+# Mutable data types are those that can be changed.
+# Mutable data types are:
+# 1. Lists
+# 2. Dictionaries
+# 3. Sets
+
+# In lists:-
+# 1. list
+
+# In dictionaries:-
+# 1. dict
+
+# In sets:-
+# 1. set
+# 2. frozenset  
