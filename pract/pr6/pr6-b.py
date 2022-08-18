11f84ad05098a27639e86b262910226dbaa01cfc
crizmo <crizmo595@gmail.com>
2022-08-18 23:18:44 +0530

    ✨ : pr6

--- pract/pr6/pr6-b.py
+++ pract/pr6/pr6-b.py
@@ -1,8 +1,21 @@
 # Write a Python program to read last n lines of a file
 
-def read_last_lines(filename, n):
-    with open(filename, 'r') as f:
-        lines = f.readlines()
-        print(lines[-n:])
+def read_last_line(file, n):
+    with open(file, 'r') as f:
+        line = f.readlines()
+        print(line[-n:])
 
-read_last_lines('/home/criz/Documents/GitHub/Py-Lab/pract/pr6/pog.txt', 1)
+        print("Displaying using for loop")
+
+        for li in line[-n:]:
+
+            '''basically how end works lol'''
+            if li.endswith('\n'):
+                li = li.split('\n')
+                print(li[0])
+
+            # print(li, end="")
+
+
+no_lines = int(input("Enter number of lines from the last line you wish to display: "))
+read_last_line('/home/criz/Documents/GitHub/py-up/pract/pr6/pog.txt', no_lines)
