b35ded1aabfb68faed8cba4286845b5113ccf71e
crizmo <crizmo595@gmail.com>
2022-10-11 23:30:34 +0530

    🗼 : better dict update

---
+++ base/dictionary/ugh.py
@@ -0,0 +1,10 @@
+thisdict = {}
+r1 = int(input("enter a range: "))
+for i in range(r1):
+
+    key = input("Enter a key: ")
+    value = input("Enter a value: ")
+
+    thisdict.update({key: value})
+
+print(thisdict)
