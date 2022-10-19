1dc0094f6418d812ddab001d7618f9858bfe8b5a
crizmo <crizmo595@gmail.com>
2022-10-19 10:08:48 +0530

    🔨 : minor fix

--- pract/pr2/pr2-c.py
+++ pract/pr2/pr2-c.py
@@ -1,8 +1,9 @@
 # Define a procedurehistogram() that takes a list of integers and prints a histogram to the screen.
-from matplotlib import pyplot as plt
+from matplotlib import pyplot
 import numpy as np
 
 a = np.array([22, 87, 5, 43, 56, 73, 55, 54, 11, 20, 51, 5, 79, 31, 27])
-fig, ax = plt.subplots(figsize=(7, 10))
+fig, ax = pyplot.subplots(figsize=(7, 10))
+
 ax.hist(a, bins=[0, 20, 40, 60, 80, 100])
-plt.show()
+pyplot.show()
